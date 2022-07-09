23ef364db07dab525d121abeeaf41fac2300c498
MadhuSudhan <chmadhus97@gmail.com>
2022-07-09 19:33:40 +0530

    added backend and provider

---
+++ backend.tf
@@ -0,0 +1,34 @@
+#Creating S3 bucket
+resource "aws_s3_bucket" "opstree-state-bucket" {
+    bucket = "opstree-state-bucket"
+    versioning {
+        enabled = true
+    }
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
+    tags = {
+        Name = "S3 Remote Terraform State Store"
+    }
+}
+#Creating dynamoDB table
+resource "aws_dynamodb_table" "Opstreelock" {
+    name           = "OpstreeD_table"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
